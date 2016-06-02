366ab7789b44221b26ed5f42b659e005175b1507
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-06-02 03:19:48 -0400

    Fixes.

--- lib/src/action.dart
+++ lib/src/action.dart
@@ -43,14 +43,11 @@ class Action {
     var result = await service.sendToControlUrl(name, param);
     var response = xml.parse(result)
       .rootElement
+      .firstChild
       .firstChild;
 
-    if (response.children.length != 1) {
-      response = response.firstChild;
-
-      if (response.children.length > 1) {
-        response = response.children[1];
-      }
+    if (response is XmlElement && !response.name.local.contains("Response")) {
+      response = response.children[1];
     }
 
     if (const bool.fromEnvironment("upnp.action.show_response", defaultValue: false)) {
